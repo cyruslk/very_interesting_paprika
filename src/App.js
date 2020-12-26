87430cb1071d648352a963bf29946ae121b16fdd
cyrus <cyrus7@hotmail.fr>
2020-12-26 01:34:56 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+  super(props);
+  this.state = {
+    loaded: false,
+    viewportHeight: null,
+    scrollHeight: null,
+    scrollable: null,
+    currentImgHeight: null,
+    maxScale: null
+  };
 }
+  componentDidMount(){
+    document.addEventListener("scroll", this.scrollHandler)
+  }
+
+
+  handleImageLoaded = () => {
+
+    this.setState({
+      loaded: true,
+      viewportHeight: window.innerHeight,
+      scrollHeight: document.documentElement.scrollHeight,
+      scrollable: this.state.scrollHeight - this.state.viewportHeight
+    }, () => {
+
+      // to optimze as refs;
+      let main_container = document.querySelector(".main_container");
+      let imgContainer = document.querySelector(".img_container");
+      let img = document.querySelector(".img_container img");
+      let currentImgHeight = img.getBoundingClientRect().height;
+      imgContainer.style.height = currentImgHeight+"px";
+
+      this.setState({
+        currentImgHeight
+      })
+    })
+  }
+
+  scrollHandler = () => {
+
+    let state = this.state;
+    if(!state.loaded){
+      return;
+    }
+
+    // to optimze as refs;
+    let main_container = document.querySelector(".main_container");
+    let imgContainer = document.querySelector(".img_container");
+
+    let img = document.querySelector(".img_container img");
+
+    let currentImgHeight = img.getBoundingClientRect().height;
+
+    let newScale = state.scrollable/(state.scrollable + 1 - document.documentElement.scrollTop);
+    let scale = (newScale>state.maxScale)?state.maxScale:newScale;
+    img.style.transform = `scaleY(${scale.toFixed(3)})`
+    imgContainer.style.height = currentImgHeight+"px"
+
+  }
+
+
+  render() {
+    return (
+      <div className="main_container">
+        <div className="main_container_inner">
+          <div className="img_container">
+            <img
+              onLoad={this.handleImageLoaded}
+              src="https://bit.ly/38xHzMk" />
+          </div>
+          <div className="text_container">
+            <h1>fvdfv</h1>
+            <p>fvdfv</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
 
 export default App;
---
+++ src/TextComponent.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class TextComponent extends Component {
+
+  render() {
+    return(
+      <div>
+        <h1>fvdfvfdvd</h1>
+      </div>
+    )
+  }
+}
+
+export default TextComponent;
